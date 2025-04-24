83007a702b8f339cb11bd1acf3ad7e638b0741c4
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2025-04-24 09:22:51 +0200

    i win

--- Clicker copy.py
+++ Clicker copy.py
@@ -37,6 +37,7 @@ def cps_add_1():
         money-=cost1
         cost1*=2
         cps_amount+=1
+        buy_upgrade.configure(text=f"{cost1}$+1 Per second")
     else:
         print
 
@@ -51,6 +52,7 @@ def cps_add_2():
         money-=cost2
         cost2*=2
         cps_amount+=2
+        buy_upgrade2.configure(text=f"{cost2}$+2 Per second")
     else:
         print
 
@@ -63,6 +65,9 @@ def cps_add_3():
         money-=cost3
         cost3*=2
         cps_amount+=5
+        buy_upgrade3.configure(text=f"{cost3}$+5 Per second")
+    else:
+        print
 
 def cps_add_4():
     global money
@@ -73,6 +78,7 @@ def cps_add_4():
         money-=cost4
         cost4*=2
         cps_amount+=10
+        buy_upgrade4.configure(text=f"{cost4}$+10 Per second")
     else:
         print
 
@@ -85,6 +91,9 @@ def cps_add_5():
         money-=cost5
         cost5*=2
         cps_amount+=20
+        buy_upgrade5.configure(text=f"{cost5}$+20 Per second")
+    else:
+        print
     
     
 
